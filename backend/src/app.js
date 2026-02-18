89cb55915e0d9d4fd17e2ebe48826031a3d995f3
ferike0078 <kemeny.ferenc.0078@gmail.com>
2026-02-18 20:59:15 +0100

    Code

---
+++ backend/src/app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import router from "./routes/post.route.js";
+import { notFound, errorHandler } from "./middlewares/error.middleware.js";
+import routerCar from "./routes/car.route.js";
+import userRouter from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //html formoknál használjuk adatkinyerésre
+
+//cookie-k olvasasa
+
+app.use(cookieParser());
+
+//cors for frontend
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ["POST", "GET", "PATCH", "DELETE", "PUT", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.get("/health", (req, res) => {
+  res.json({ ok: true });
+});
+
+app.use("/api/v1/cars", routerCar);
+app.use("/api/v1/users", userRouter);
+
+app.use("/api/v1/posts", router); //alapja a postoknak, utána a router elválasztja azokat
+app.use(notFound); //ha nem jo az url eleres
+app.use(errorHandler); //központi error handler --> minden next(err)-el tovabadott hiba ide erkezik
+
+export default app;
